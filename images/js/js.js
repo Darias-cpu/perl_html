6e4c757b0bc5c2514d2984aa5133c5b6bdf10dcd
Darias-cpu <darias24.gathungu@student.cuk.ac.ke>
2024-10-16 11:15:45 +0100

    initialcommit

---
+++ images/js/js.js
@@ -0,0 +1,8 @@
+const colorButton:=document.getElementById(elementId:'colorButton')
+.addEventListener('click',Listener()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor==='blue'?'4CAF50':'blue';
+
+}
+
+);
+
